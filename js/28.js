d248c30f97ec6acca4c87303cd41afc3e36d0684
Angle <45601608+3rw4t@users.noreply.github.com>
2025-08-08 17:40:07 -0300

    Selectores

---
+++ js/28.js
@@ -0,0 +1,12 @@
+// Selectores
+
+const heading = document.querySelector('.heading') // trae hasta un elemento
+console.log(heading);
+console.log(heading.tagName);
+console.log(heading.textContent)
+console.log(heading.id)
+console.log(heading.classList)
+
+const enlaces = document.querySelectorAll('.navegacion a') // trae todos los elementos que correspondan al selector
+console.log(enlaces)
+
