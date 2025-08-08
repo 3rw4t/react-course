bfb53798bf2f8c01bba723a92ca9a4eb7bbbe7e9
Angle <45601608+3rw4t@users.noreply.github.com>
2025-08-08 12:35:59 -0300

    Introduccion a ECMAScript Modules (24.js y funciones.js)

---
+++ js/24.js
@@ -0,0 +1,7 @@
+// Desventajas de un archivo muy grande, ralentiza la navegacion, 
+//por lo tanto podríamos importar funciones de otros archivos, logrando tambien que el codigo sea mas facil de mantener
+
+import {sumar, restar} from './funciones.js'
+const resultado = sumar(20,30)
+
+console.log(resultado)
