f16654d60b0decc5e16ca8f040904e70ba6573f3
Angle <45601608+3rw4t@users.noreply.github.com>
2025-08-08 13:49:17 -0300

    Export default in js

--- js/24.js
+++ js/24.js
@@ -1,8 +1,15 @@
 // Desventajas de un archivo muy grande, ralentiza la navegacion, 
 //por lo tanto podríamos importar funciones de otros archivos, logrando tambien que el codigo sea mas facil de mantener
 
-import {sumar as funcionSumar, restar} from './funciones.js'//importando funcion con alias
-const resultado = funcionSumar(20,30)
+import hola, {sumar as funcionSumar, restar, dividir} from './funciones.js'//importando funcion con alias
 
-console.log(resultado)
+const resultado1 = hola(20,30)
+const resultado2 = funcionSumar(20,30)
+const resultado3 = restar(20,30)
+const resultado4 = dividir(20,30)
+
+console.log(resultado1)
+console.log(resultado2)
+console.log(resultado3)
+console.log(resultado4)
 
--- js/funciones.js
+++ js/funciones.js
@@ -1,17 +1,17 @@
 // Siempre hay que tener el type = "module" para poder importar las funciones de otro archivo!!
 
- function sumar(n1,n2) {
-    return n1 + n2
-}
+
+ export const sumar = (n1,n2) => n1 + n2 //también se pueden exportar arrow functions!
 
 function restar(n1,n2){
     return n1 - n2
 }
 
+const dividir = (n1 , n2) => n1 / n2
 
-export {
-    sumar,
-    restar
+export { // para exportar multiples funciones y no tener que usar la instruccion export en cada una
+    restar,
+    dividir
 }
 
 
@@ -25,4 +25,15 @@ export {
 // export function restar(n1,n2){
 //     return n1 - n2
 // }
+
+
+//con esto denotamos la funcion default a exportar ie import multiplicar, { ...} ...
+// export default function multiplicar(n1,n2){
+//     return n1*n2
+// }
+
+//para exportar de forma default una arrow function se haría algo así
+
+const multiplicar = (n1, n2) => n1 * n2
+export default multiplicar
 
