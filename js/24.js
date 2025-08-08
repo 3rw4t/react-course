f1521e012078e258a8f468d5ca0e6be17271ac01
Angle <45601608+3rw4t@users.noreply.github.com>
2025-08-08 12:45:21 -0300

    Mas sobre modulos, distintas formas de exportar funciones

--- js/24.js
+++ js/24.js
@@ -1,7 +1,8 @@
 // Desventajas de un archivo muy grande, ralentiza la navegacion, 
 //por lo tanto podríamos importar funciones de otros archivos, logrando tambien que el codigo sea mas facil de mantener
 
-import {sumar, restar} from './funciones.js'
-const resultado = sumar(20,30)
+import {sumar as funcionSumar, restar} from './funciones.js'//importando funcion con alias
+const resultado = funcionSumar(20,30)
 
 console.log(resultado)
+
--- js/funciones.js
+++ js/funciones.js
@@ -1,9 +1,28 @@
-export function sumar(n1,n2) {
+// Siempre hay que tener el type = "module" para poder importar las funciones de otro archivo!!
+
+ function sumar(n1,n2) {
     return n1 + n2
 }
 
-export function restar(n1,n2){
+function restar(n1,n2){
     return n1 - n2
 }
 
+
+export {
+    sumar,
+    restar
+}
+
+
+
+// Tambien podrían exportarse así
+
+// export function sumar(n1,n2) {
+//     return n1 + n2
+// }
+
+// export function restar(n1,n2){
+//     return n1 - n2
+// }
 
