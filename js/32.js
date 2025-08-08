09069cfab5c64eece9f2efd2f3e5349c2a448e4a
Angle <45601608+3rw4t@users.noreply.github.com>
2025-08-08 19:34:03 -0300

    Validando formularios y mostrando alertas

--- js/32.js
+++ js/32.js
@@ -2,17 +2,31 @@
 
 const formulario = document.querySelector('#formulario')
 formulario.addEventListener('submit', (e) => {
+
     e.preventDefault()
     const nombre = document.querySelector('#nombre').value
-    
     const password = document.querySelector('#password').value
-    console.log(nombre);
-    console.log(password);
     
+    //prevenir nuevas alertas
+    const alertaPrevia = document.querySelector('.alerta')
+    alertaPrevia?.remove() //si existe la remueve, sino, no hace nada :o
+
+
+    const alerta = document.createElement('DIV')
+    
+    alerta.classList.add('alerta', 'text-white', 'uppercase', 'text-sm', 'text-center', 'p-2', 'font-black')   //alerta.classList = "clase-1 clase-2"
+
+
     if (nombre === '' || password === ''){ //validacion sencilla!
-        console.log('todos los campos son obligatorios')
+
+        alerta.textContent = 'todos los campos son obligatorios'
+        alerta.classList.add('bg-red-500')
+
     } else {
-        console.log('iniciando sesion')
+        alerta.textContent = 'iniciando sesion'
+        alerta.classList.add('bg-green-500')
     }
-    console.log('enviando formulario')
+    formulario.appendChild(alerta) //le agregamos un hijo a formulario!
+
+    setTimeout(() => alerta.remove(), 3000)
 })
